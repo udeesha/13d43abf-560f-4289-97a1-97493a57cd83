e7bc338f0930cc9ef1b59003bef9c01cd536934f
Udeesha Wijenayaka <udeeshaw@hotmail.com>
2026-03-14 23:13:45 +1100

    Add project files.

---
+++ PatternHandler/Helper/Utility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace PatternHandler.Helper;
+
+/// <summary>
+/// Provides spporting methods for SequenceChecker class
+/// </summary>
+public static class Utility
+{
+    /// <summary>
+    /// Check the format of the input text. Accept only numbers and spaces. Multiple spaces are accepted
+    /// </summary>
+    /// <param name="inputText">Eg:- 5 6 1 6</param>
+    /// <returns>True- only numbers and spaces are found, else returns false</returns>
+    public static bool IsValidInput(string inputText)
+    {
+        string regexFormat = @"(\s*[0-9]+)+";
+        return Regex.Match(inputText, regexFormat).Success;
+    }
+
+    /// <summary>
+    /// Converts space delimited number string into an IEnumerable int list 
+    /// </summary>
+    /// <param name="inputText">Eg:- 5 6 1 6</param>
+    /// <returns>IEnumerable int list</returns>
+    public static IEnumerable<int> GetNumberList(string inputText, string delimeter)
+    {
+        return Array.ConvertAll(inputText.Split(delimeter, StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s));
+    }
+
+    /// <summary>
+    /// Add given int values into temp sequence list. It will add previousNumber only for empty sequence list and add currentNumber all the time
+    /// </summary>
+    /// <param name="sequence">Accept reference to sequence list</param>
+    /// <param name="previousNumber">Previous mumber in pair</param>
+    /// <param name="currentNumber">next number in the pair</param>
+    /// <returns>Returns reference for updated sequence list</returns>
+    public static List<int> AddNumberToTempSequence(List<int> sequence, int previousNumber, int currentNumber)
+    {
+        if (sequence.Count == 0)
+            sequence.Add(previousNumber);
+
+        sequence.Add(currentNumber);
+
+        return sequence;
+    }
+
+
+    /// <summary>
+    /// Compare tempSequence list with sequence list. Always returns the longest sequence list 
+    /// </summary>
+    /// <param name="tempSequence">Reference to temp sequence list</param>
+    /// <param name="longestSequence">Reference to longest sequece list</param>
+    /// <returns>Returns reference for updated sequence list</returns>
+    public static List<int> CompareSequences(List<int> tempSequence, List<int> sequence)
+    {
+        //Add tempSequence items into sequence when sequence is empty
+        if (sequence.Count == 0)
+        {
+            sequence.AddRange(tempSequence);
+        }
+        //compare tempSequence with existing sequence and only update if it is longer that existing one
+        else if (sequence.Count < tempSequence.Count)
+        {
+            sequence.Clear();
+            sequence.AddRange(tempSequence);
+        }
+
+        return sequence;
+    }
+}
---
+++ PatternHandler/Services/SequenceChecker.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using PatternHandler.Helper;
+
+namespace PatternHandler.Services;
+
+/// <summary>
+/// This class contains a method to find longest incremental sub sequence from given number sequence
+/// </summary>
+public static class SequenceChecker
+{
+
+    //Set input numbers delimiter
+    private const string DELIMITER = " ";
+
+    /// <summary>
+    /// Finds the longest increasing subsequence (increased by any number) present in given sequence
+    /// If more than 1 sequence exists with the longest length, return the earliest sequence
+    /// </summary>
+    /// <param name="inputText">Accept string with numbers and spaces</param>
+    /// <returns>
+    /// Valid input -> longest increamental sequence
+    /// Incorrect input -> "Incorrect format"       
+    /// </returns>
+    public static string GetLongestSubSequence(string inputText)
+    {
+        //Return error code for incorrect format of input string
+        if (!Utility.IsValidInput(inputText))
+        {
+            return "Incorrect format";
+        }
+        //Converts string list into int list
+        IEnumerable<int> itemList = Utility.GetNumberList(inputText, DELIMITER);
+        var enumerator = itemList.GetEnumerator();
+
+        //Move to fisrt record and get first element/number as previousNumber
+        enumerator.MoveNext();
+        int previousNumber = enumerator.Current; // Placeholder to store previous number while comparing number pairs in sequence loop
+
+        List<int> longestSequence = new List<int>(); //Placeholder to store the longest incremental sequence list. 
+        List<int> tempSequence = new List<int>(); //Placeholder to keep and append incremental sequence list while looping through the complete list
+
+        //looping through the list from second element onwards
+        while (enumerator.MoveNext())
+        {
+            //If current number pair is an incremental sequence, it will be added into tempSequence list until current pair is not an incremental pair 
+            if (previousNumber < enumerator.Current)
+                Utility.AddNumberToTempSequence(tempSequence, previousNumber, enumerator.Current);
+            //When current pair is not an increamental pair, it will check tempSequence list and add that into longestSequence list. If tempSequence list is empty, it will move to next pair
+            else
+                longestSequence = AddLongestSequence(tempSequence, longestSequence);
+
+            //Update current number as previous number to compare with next number from main sequence
+            previousNumber = enumerator.Current;
+        }
+        //Check and add if there is a longest sequence at the end of the list (including the last item. Need to check this outside the loop as last iteration is not checking that)
+        longestSequence = AddLongestSequence(tempSequence, longestSequence);
+
+        //Returns the longest sequence as a string
+        return string.Join(" ", longestSequence);
+
+    }
+
+    /// <summary>
+    /// Check given temp increasing sequence against stored longest sequence and update it if temp on is longer that stored one
+    /// </summary>
+    /// <param name="tempSequence">Reference to temp sequence list</param>
+    /// <param name="longestSequence">Reference to longest sequece list</param>
+    /// <returns>longestSequence</returns>
+    private static List<int> AddLongestSequence(List<int> tempSequence, List<int> longestSequence)
+    {
+        if (tempSequence.Count > 1)
+        {
+            //Check and update longest sequence list
+            longestSequence = Utility.CompareSequences(tempSequence, longestSequence);
+
+            //Clear items from tempSequence list to start appending values for  next incremental sequence
+            tempSequence.Clear();
+        }
+        return longestSequence;
+    }
+}
