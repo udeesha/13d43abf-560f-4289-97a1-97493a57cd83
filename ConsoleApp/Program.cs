7472190d413db113703449977206fc5adcfc61d1
Udeesha Wijenayaka <udeeshaw@hotmail.com>
2026-03-14 23:22:19 +1100

    Console app changes to test the SequenceChecker

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PatternHandler.Services;
+
+Console.WriteLine("Find the longest increasing subsequence");
+string inputText = "6 1 5 9 2";
+Console.WriteLine($"Input - {inputText}");
+
+string outputText = SequenceChecker.GetLongestSubSequence(inputText);
+Console.WriteLine($"OutPut - {outputText}");
+
+
+//Find the longest increasing subsequence
+//Input - 6 1 5 9 2
+//OutPut - 1 5 9
